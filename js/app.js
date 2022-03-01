3593ce1dac502c2a6cf503416e7f3912f6f0ecdd
shorifuddin2 <shorifuddin240915@gmail.com>
2022-03-01 16:39:50 +0600

    project css or js errow function creade

--- js/app.js
+++ js/app.js
@@ -1,58 +1,93 @@
-const searchAll = () =>{
-    
- const searchText = document.getElementById('search-value').value;
- const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => sowDisplay(data.data))
+const inputFiled = document.getElementById("input-fild");
+const searchButton = document.getElementById("search-button");
+const cardGroup = document.getElementById("card-group");
+const detailsDiv = document.getElementById("detailes-div");
+const cardSection = document.getElementById("card-section");
 
-    document.getElementById("search-value").value = "";
-    
-}
+// ====================>>> Click Handler Fun
+searchButton.addEventListener("click", () => {
+    const inputValue = inputFiled.value;
+    inputFiled.value = "";
+    cardGroup.textContent = "";
+    detailsDiv.textContent = "";
 
-const sowDisplay = (phones) =>{
-    for(const data of phones){
-        const card = document.getElementById('card');
-        let div = document.createElement("div");
-        div.classList.add("col-md-4");
-        div.innerHTML =`
-        <div onclick="sowDisplay(${data.slug})"class="card m-2 p-5 border=0 shadow rounded-3">
-        <img src="${data.image}"class="card-img-top">
-        <div class="card-body">
-        <h5 class="card-title">Brand: ${data.brand}</h5>
-        <h6 class="card-title">Name: ${data.phone_name}</h6>
-        <button  type="submit" class="btn btn-success">Phone Details</button>
-        </div>
-        </div>
-        `
-        
-        card.appendChild(div)
+    if (isNaN(inputValue)) {
+        searchPhoneFun(inputValue);
     }
+    else {
+        alert("Please enter a valid name");
+    }
+})
+
+// ====================>>> Data fetch (All Phones)
+const searchPhoneFun = (phoneName) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(phone => phonesConditionFun(phone.data))
+};
+searchPhoneFun("phone");
+
+const phonesConditionFun = (phones) => {
+    if (phones.length == 0) {
+        alert("Please enter a valid name");
+    }
+    else {
+        const phonses = phones.slice(0, 20);
+        phonses.forEach(phone => {
+            const div = document.createElement("div");
+            div.classList.add("col");
+            div.innerHTML = ` 
+                <div class="card">
+                    <img src="${phone.image}" class="card-img-top phone-style p-5" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Phone Brand: ${phone.brand}</h5>
+                        <p class="card-text text-center"> <span>Name:</span> ${phone.phone_name}.</p>
+                    </div>
+                    <button id="detals-btn" class="bg-primary" onclick="seeMoreDetails('${phone.slug}')"  >More Details</button>
+                </div>
+            `;
+            cardGroup.appendChild(div);
+        })
+    }
+};
+// ================>>> Data fetch with Id
+const seeMoreDetails = (phoneId) => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => moreInfoFun(data.data))
 }
 
-// const phoneDetals = (phoneDetals) =>{
-//     const url=`https://openapi.programming-hero.com/api/phone/${phoneDetals}`
-//     console.log(url)
-//     // fetch(url)
-//     // .then(res => res.json())
-//     // .then(data => console.log(data))
-// }
-
-// const showPhoneDetail = (data) =>{
-// // console.log(data);
-// const detalsPhone = document.getElementById('detalsPhone')
-// const div = document.createElement('div');
-// div.classList.add('card');
-// div.innerHTML=`
-// <img src="${data.image}"class="card-img-top" alt="Phone">
-// <div class="card-body">
-// <h5 class="card-title">${data.mainFeatures.name}</h5>
-// <p class="card-text">Release Date: ${data.mainFeatures.releaseDate}</p>
-// <p class="card-text">Main Features: ${data.mainFeatures.chipSet}</p>
-// <p class="card-text">Memory: ${data.mainFeatures.memory}</p>
-// <p class="card-text">Display Size: ${data.mainFeatures.displaySize}</p>
-// <p class="card-text">ID: ${data.slug}</p>
-// </div>
-// `
-// detalsPhone.appendChild(div)
-// }
+// ============>>>Show Detailes Fun
+const moreInfoFun = (moreInfo) => {
+    detailsDiv.textContent = "";
+    console.log(moreInfo);
+    const div = document.createElement("div");
+    div.classList.add("details-style-div")
+    div.innerHTML = ` 
+            <div class="row g-0">
+                <div class="col-md-5">
+                    <img src="${moreInfo.image}" class="img-fluid rounded-start p-5 detals-image-style" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                        <h5 class="card-title">${moreInfo.brand}</h5>
+                        <p class="card-text">${moreInfo.releaseDate ? moreInfo.releaseDate : "No release date found"}</p>
+                        <h6>Features</h6>
+                        <p class="card-text"><span>ChipSet</span> : <small>${moreInfo.mainFeatures.chipSet}</small></p>
+                        <p class="card-text"><span>DisplaySize</span> : <small>${moreInfo.mainFeatures.displaySize}</small></p>
+                        <p class="card-text"><span>Memory</span> : <small>${moreInfo.mainFeatures.memory}</small></p>
+                        <p class="card-text"><span>Sensors</span> : <small>${moreInfo.mainFeatures.sensors}</small></p>
+                        <p class="card-text"><span>Others</span> :<br> 
+                          Bluetooth :<small>${moreInfo?.others?.Bluetooth ? moreInfo?.others?.Bluetooth : "No more information"}</small>
+                          GPS :<small>${moreInfo?.others?.GPS ? moreInfo?.others?.GPS : "No more information"}</small>
+                          NFC :<small>${moreInfo?.others?.NFC ? moreInfo?.others?.NFC : "No more information"}</small>
+                          
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+    detailsDiv.appendChild(div)
+}
