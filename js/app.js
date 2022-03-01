e070fd993568f82660d984b3fa9bc1cb6ac582ff
shorifuddin2 <shorifuddin240915@gmail.com>
2022-03-01 18:20:47 +0600

    fantional errow hendelet

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const inputFiled = document.getElementById("input-fild");
+const inputFiled = document.getElementById("input-filed");
 const searchButton = document.getElementById("search-button");
 const cardGroup = document.getElementById("card-group");
 const detailsDiv = document.getElementById("detailes-div");
@@ -56,33 +56,33 @@ const seeMoreDetails = (phoneId) => {
     const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => moreInfoFun(data.data))
+        .then(data => moreDetalsFun(data.data))
 }
 
 // ============>>>Show Detailes Fun
-const moreInfoFun = (moreInfo) => {
+const moreDetalsFun = (detalsInfo) => {
     detailsDiv.textContent = "";
-    console.log(moreInfo);
+    console.log(detalsInfo);
     const div = document.createElement("div");
     div.classList.add("details-style-div")
     div.innerHTML = ` 
             <div class="row g-0">
                 <div class="col-md-5">
-                    <img src="${moreInfo.image}" class="img-fluid rounded-start p-5 detals-image-style" alt="...">
+                    <img src="${detalsInfo.image}" class="img-fluid rounded-start p-5 detals-image-style" alt="...">
                 </div>
                 <div class="col-md-7">
                     <div class="card-body">
-                        <h5 class="card-title">${moreInfo.brand}</h5>
-                        <p class="card-text">${moreInfo.releaseDate ? moreInfo.releaseDate : "No release date found"}</p>
+                        <h5 class="card-title">${detalsInfo.brand}</h5>
+                        <p class="card-text">${detalsInfo.releaseDate ? detalsInfo.releaseDate : "No release date found"}</p>
                         <h6>Features</h6>
-                        <p class="card-text"><span>ChipSet</span> : <small>${moreInfo.mainFeatures.chipSet}</small></p>
-                        <p class="card-text"><span>DisplaySize</span> : <small>${moreInfo.mainFeatures.displaySize}</small></p>
-                        <p class="card-text"><span>Memory</span> : <small>${moreInfo.mainFeatures.memory}</small></p>
-                        <p class="card-text"><span>Sensors</span> : <small>${moreInfo.mainFeatures.sensors}</small></p>
+                        <p class="card-text"><span>ChipSet</span> : <small>${detalsInfo.mainFeatures.chipSet}</small></p>
+                        <p class="card-text"><span>DisplaySize</span> : <small>${detalsInfo.mainFeatures.displaySize}</small></p>
+                        <p class="card-text"><span>Memory</span> : <small>${detalsInfo.mainFeatures.memory}</small></p>
+                        <p class="card-text"><span>Sensors</span> : <small>${detalsInfo.mainFeatures.sensors}</small></p>
                         <p class="card-text"><span>Others</span> :<br> 
-                          Bluetooth :<small>${moreInfo?.others?.Bluetooth ? moreInfo?.others?.Bluetooth : "No more information"}</small>
-                          GPS :<small>${moreInfo?.others?.GPS ? moreInfo?.others?.GPS : "No more information"}</small>
-                          NFC :<small>${moreInfo?.others?.NFC ? moreInfo?.others?.NFC : "No more information"}</small>
+                          Bluetooth :<small>${detalsInfo?.others?.Bluetooth ? detalsInfo?.others?.Bluetooth : "No more information"}</small>
+                          GPS :<small>${detalsInfo?.others?.GPS ? detalsInfo?.others?.GPS : "No more information"}</small>
+                          NFC :<small>${detalsInfo?.others?.NFC ? detalsInfo?.others?.NFC : "No more information"}</small>
                           
                         </p>
                     </div>
